270236378133377844f3791db8f902c75ec5729e
Edoardo Tombolesi <edoardotombolesi8@gmail.com>
2025-04-10 01:15:54 -0300

    adição projeto

---
+++ src/main/java/com/pyedoardo/ed_java/ED_Java.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.pyedoardo.ed_java;
+
+/**
+ *
+ * @author edoar
+ */
+public class ED_Java {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
